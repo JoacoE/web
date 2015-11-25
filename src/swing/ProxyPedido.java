b6a24e5cba6c2dec026c5abf2a325e196146b369
joaco <joaco@TimelineX>
2015-11-25 01:33:52 -0200

    Se mejoro la lectura de los archivos de configuracion

--- src/swing/ProxyPedido.java
+++ src/swing/ProxyPedido.java
@@ -81,8 +81,8 @@ public class ProxyPedido {
             if(!existCfgFile()){
                 createCfgFile();
             }
-            String pathArchivo = jarDir+"/"+propertiesFile;
-            input = new FileInputStream(pathArchivo);
+            File Archivo = new File(jarDir,propertiesFile);
+            input = new FileInputStream(Archivo);
             prop.load(input);
             String ip = prop.getProperty("Ip");
             String puerto = prop.getProperty("Port");
